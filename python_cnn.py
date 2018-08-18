0092773898e3d3689d525c74dca8a79fd604740c
Masoud S. Nosrati <smasoudn@gmail.com>
2018-08-18 19:18:01 -0400

    Completed!

--- python_cnn.py
+++ python_cnn.py
@@ -21,6 +21,7 @@ def d_sigmoid(x):
 
 
 
+
 # Generate inputs
 #############################################
 def generate_inputs():
@@ -45,7 +46,9 @@ def generate_inputs():
                     [0,0,1,0],
                     [1,1,1,0]])
     Y = [0.0, 1.0, 2.0, 3.0]
+    Y = [Y[i] / len(Y) for i in range(len(Y))]
     X = [x1, x2, x3, x4]
+
     return X, Y
 
 # Initial weights
@@ -57,7 +60,7 @@ w3 = np.random.randn(12, 1) * 4
 # Hyper parameter setting
 #############################################
 num_epochs = 10000
-learning_rate = 0.001
+learning_rate = 0.1
 
 
 # Training
@@ -80,17 +83,18 @@ for ep in range(num_epochs):
         cost =  np.square(layer_3 - Y[i]).sum() * 0.5
         total_cost += cost
 
-        delta_3 = (layer_3 - Y[i]) * d_sigmoid(layer_3)
-        delta_2 = np.reshape(delta_3.dot(w3.T), (4,3)) * d_sigmoid(layer_2)
-        delta_1 = delta_2 * w2 * d_sigmoid(layer_1)
+        delta_3 = (layer_3 - Y[i]) * sigmoid(layer_3) * (1 - sigmoid(layer_3))
+        delta_2 = np.reshape(delta_3.dot(w3.T), (4, 3))  * sigmoid(layer_2) * (1 - sigmoid(layer_2))
+        delta_1 = delta_2 * w2 * sigmoid(layer_1) * (1  - sigmoid(layer_1))
 
         del_3 = layer_2_flattened.T.dot(delta_3)
-        del_2 = layer_1 * delta_2
+        del_2 = np.rot90(signal.convolve2d(layer_1, np.reshape(delta_2, (4,3)), 'valid'), 2)
         del_1 = np.rot90(signal.convolve2d(X[i], np.rot90(delta_1, 2), 'valid'), 2)
 
         w3 = w3 - learning_rate * del_3
         w2 = w2 - learning_rate * del_2
         w1 = w1 - learning_rate * del_1
+    print(total_cost)
 
 
 for i in range(len(X)):
@@ -104,6 +108,6 @@ for i in range(len(X)):
     layer_3 = layer_2_flattened.dot(w3)
     layer_3 = sigmoid(layer_3)
 
-    print(layer_3[0][0], "  ", Y[i])
+    print(layer_3[0][0] * len(Y), "  ", Y[i] *  len(Y))
 
 
